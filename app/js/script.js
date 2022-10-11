f39ef17e9a67bf8e4a73a07bd3487a2a8bee6a25
Blurryface1998 <83365791+Blurryface1998@users.noreply.github.com>
2022-10-11 17:08:39 +0200

    Fix Part 2

--- app/js/script.js
+++ app/js/script.js
@@ -1,62 +1,34 @@
-const radioBG = document.getElementById("radioBG");
-const radioBtn = document.querySelectorAll("input[type=radio]");
+
 const darkBtn = document.getElementById("darkM");
-const lightkBtn = document.getElementById("lightM");
-const toggleBtn = document.getElementById("toggleBtn");
-const toggle = document.getElementById("toggle");
-const body = document.getElementById("body");
-const header = document.getElementById("headerId");
-const sectionH2 = document.getElementById("overviewId");
+const ligthBtn = document.getElementById("lightM");
+const body = document.querySelector("body");
+const radioBtns = document.querySelectorAll(".toggle__wrapper");
 
+const setLightMode = () => {
+    body.classList = "light";
+};
 
-toggle.addEventListener("mouseover", () => {
-    radioBG.classList.add("hover");
-});
+const setDarkMode = () => {
+    body.classList = "dark";
+};
 
-toggle.addEventListener("mouseleave", () => {
-    radioBG.classList.remove("hover");
-});
+const preferColorScheme = () => {
+    return window.matchMedia("(prefers-color-scheme: light)").matches
+    ? "light" 
+    : "dark"
+};
 
+console.log(preferColorScheme());
 
-for(let i = 0; i < radioBtn.length; i++) {
-    radioBtn[i].addEventListener("click", () => {       
-        if(darkBtn.checked) {
-            body.classList.add("dark");
-            radioBG.classList.add("darkRadioBG");
-            toggleBtn.classList.add("darkToogleBtn");
-            transitionE();
-
-        } else if(!darkBtn.checked) {
-            body.classList.remove("dark");
-            radioBG.classList.remove("darkRadioBG");
-            toggleBtn.classList.remove("darkToogleBtn");
-            transitionE();
-        }
+// Changing body color when radio btn is clicked
+for(let i = 0; i < radioBtns.length; i++) {
+    radioBtns[i].addEventListener("click", (event) => {
+        ligthBtn.checked ? setLightMode() : setDarkMode();
     });
 }
 
-function transitionE() {
-    let h1 = document.querySelector("h1");
-    let h2 = document.querySelectorAll("h2");
-    let h3 = document.querySelectorAll("h3");
-    let cards = document.querySelectorAll(".card");
-    
-    let transition = getComputedStyle(document.documentElement)
-    .getPropertyValue("--transition");
 
-    body.style.transition = transition;
-    header.style.transition = transition;
-    h1.style.transition = transition;
-
-    for(let i = 0; i < h2.length; i++) {
-        h2[i].style.transition = transition;
-    }
-
-    for(let i = 0; i < h3.length; i++) {
-        h3[i].style.transition = transition;
-    }
-
-    for(let i = 0; i < cards.length; i++) {
-        cards[i].style.transition = transition;
-    }
-}
+window.matchMedia("(prefers-color-scheme: light)")
+    .addEventListener("change", (event) => {
+        event.matches ? ligthBtn.click() : darkBtn.click();
+    });
